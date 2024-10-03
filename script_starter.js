888c41c371c2b00d345bdc38636bb1eb35e2217b
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-10-03 10:07:33 +0000

    A new commit

--- script_starter.js
+++ script_starter.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btnOpen => btnOpen.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,26 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+
+const header = document.querySelector('.header');
+
+// recuperation d'une classe par son nom
+//document.getElementsByClassName('.btn');
+
+//creation et insertion  d'element dans le HTML
+
+//insertAddjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML = `
+We use cooki for imporved functionnality and analytics
+<button class="btn btn--close-cookie"> Got it! </button>
+`;
+header.prepend(message);
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+//document.documentElement.style.setProperty("--color-primary", "orange")
